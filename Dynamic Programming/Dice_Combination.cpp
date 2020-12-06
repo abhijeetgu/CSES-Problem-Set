b1993b506566c81ff7f1a6dda453dd7df787a6a6
Abhijeet Kumar <49158711+abhijeetgu@users.noreply.github.com>
2020-12-07 01:03:11 +0530

    Update Dice_Combination.cpp

--- Dynamic Programming/Dice_Combination.cpp
+++ Dynamic Programming/Dice_Combination.cpp
@@ -43,3 +43,45 @@ int main()
  
     return 0;
 }
+
+
+--------------------------------------------------------------------------------------------------------------------
+ 
+ #include<bits/stdc++.h>
+using namespace std;
+ 
+#define fast_io  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+ 
+int main()
+{
+    fast_io
+    int n,x;
+    cin>>n>>x;
+    vector<int> coins(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>coins[i];
+    }
+    vector<int> dp(x+1,1e9);
+    dp[0] =0;
+ 
+ 
+    for(int i=1;i<=x;i++)
+    {
+ 
+        for(int j=0;j<n;j++)
+        {
+          if(i>=coins[j])
+          {
+             dp[i] = min(dp[i],dp[i-coins[j]]+1);
+          }
+        }
+ 
+    }
+    cout << (dp[x] == 1e9 ? -1 : dp[x]) << endl;
+ 
+    return 0;
+}
+ 
+ 
